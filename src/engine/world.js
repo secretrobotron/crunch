0b21592c745ed222aed3fc7dacd64bb9db66c556
Nicolas Silva <nical.silva@gamil.com>
2012-05-11 01:10:38 -0400

    [world] added world.js

---
+++ src/engine/world.js
@@ -0,0 +1,12 @@
+
+
+// a place to put the game settings and world variables
+define(function(){
+    world = {
+          gravity:10
+        , friction:1
+    };
+
+
+    return world;
+});//module world
